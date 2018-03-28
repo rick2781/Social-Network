0a8e55d236da277789a850d177d176626a12f538
Rick <ricksofteng@gmail.com>
2018-03-28 14:37:07 -0700

    Toolbar/Fab Added

--- app/src/main/java/e/rick/social_network/Presentation/HomeActivity/HomeActivity.kt
+++ app/src/main/java/e/rick/social_network/Presentation/HomeActivity/HomeActivity.kt
@@ -4,22 +4,25 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
 import e.rick.social_network.Model.PostObj
 import e.rick.social_network.R
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.main_toolbar.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        setSupportActionBar(toolbar)
 
         val postObj = PostObj("xaxa", "to sarrando na xaxa", null)
 
         var listItem = ArrayList<PostObj>()
         listItem.add(postObj)
 
-        recyclerView.adapter = PostAdapter(listItem)
+        recyclerView.adapter = PostAdapter(listItem, this)
         recyclerView.layoutManager = LinearLayoutManager(this) as RecyclerView.LayoutManager?
     }
 }
--- app/src/main/java/e/rick/social_network/Presentation/HomeActivity/PostAdapter.kt
+++ app/src/main/java/e/rick/social_network/Presentation/HomeActivity/PostAdapter.kt
@@ -1,17 +1,20 @@
 package e.rick.social_network.Presentation.HomeActivity
 
+import android.content.Context
+import android.content.res.Resources
 import android.support.constraint.ConstraintLayout
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import e.rick.social_network.Model.PostObj
 import e.rick.social_network.R
 import kotlinx.android.synthetic.main.list_item.view.*
 import org.jetbrains.anko.find
 
-class PostAdapter (private val listItem : ArrayList<PostObj>): RecyclerView.Adapter<PostAdapter.ViewHolder>() {
+class PostAdapter (private val listItem : ArrayList<PostObj>, private val context : Context): RecyclerView.Adapter<PostAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostAdapter.ViewHolder {
 
@@ -30,7 +33,11 @@ class PostAdapter (private val listItem : ArrayList<PostObj>): RecyclerView.Adap
 
         holder.username?.text = post.username
 
+        post.imageBody?.let {
 
+            val imageBody = ImageView(context)
+//            imageBody.setImageResource(post.imageBody)
+        }
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
